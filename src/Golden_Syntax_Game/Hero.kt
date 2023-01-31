08f4127250ea25478974922df670631155f7f651
mirel.apetrei <mirel_apetrei@icloud.com>
2023-01-31 15:49:19 +0100

    1.3 -> class Samurai dweleted

--- src/Golden_Syntax_Game/Hero.kt
+++ src/Golden_Syntax_Game/Hero.kt
@@ -1,8 +1,20 @@
 package Golden_Syntax_Game
 
-open class Hero {
-    val name: String? = null
+open class Hero() {
+
     val type: String? = null
     var healthPoints: Int? = null
+    var powerLevel: Int? = null
+    var damagePower: Int? = null
 
+
+    open fun attack() {
+
+    }
+
+    open fun heal() {}
+
+   open fun defense() {}
+
+    open fun pass() {}
 }
--- src/Golden_Syntax_Game/Paladin.kt
+++ src/Golden_Syntax_Game/Paladin.kt
@@ -1,4 +1,9 @@
 package Golden_Syntax_Game
 
 class Paladin(): Hero() {
+    var weaponTpe: String? = null
+
+    override fun attack(){
+
+    }
 }
