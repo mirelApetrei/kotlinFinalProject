5b4b0e373e967852ad0b49db15ec9622ce67eebc
mirel.apetrei <mirel_apetrei@icloud.com>
2023-02-01 15:44:46 +0100

    1.5 -> Ranger class added

---
+++ src/Golden_Syntax_Game/WariorsType/Ranger.kt
@@ -0,0 +1,11 @@
+package Golden_Syntax_Game.WariorsType
+
+import Golden_Syntax_Game.Hero
+
+class Ranger(heroName: String,
+             healthPoints: Int?,
+             level: Int?,
+             damagePower: Int?): Hero(heroName, healthPoints, level, damagePower) {
+
+
+}
