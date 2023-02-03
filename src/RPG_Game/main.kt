db42dd307957e4808d809e99b4abf7f57e0e6768
mirel.apetrei <mirel_apetrei@icloud.com>
2023-02-03 16:29:51 +0100

    2.2 -> Player class worked + Game.class

--- src/RPG_Game/main.kt
+++ src/RPG_Game/main.kt
@@ -4,15 +4,9 @@ import RPG_Game.WariorsType.*
 
 fun main(args: Array<String>) {
 
-
-
-//    var myTeam: PlayerTeam = PlayerTeam()
-//    myTeam.teamBuilding()
-//    println(myTeam.toString())
-//
-//    var myTeam2: PlayerTeam = PlayerTeam()
-//    myTeam2.teamBuilding()
-//    println(myTeam2.toString())
+    var myTeam: PlayerTeam = PlayerTeam()
+    myTeam.teamBuilding()
+    println(myTeam.toString())
 
     var myTeam3: PlayerTeam = PlayerTeam()
     myTeam3.teamBuilding()
